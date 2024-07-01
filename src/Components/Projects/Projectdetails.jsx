eb6274b468842432e102348b519265dc65e56817
madhuravani7 <madhubudige67@gmail.com>
2024-07-01 17:10:32 +1000

    level4 of my site

---
+++ src/Components/Projects/Projectdetails.jsx
@@ -0,0 +1,27 @@
+const projectdetails = [
+    {
+        key: 1,
+        src: process.env.PUBLIC_URL + '/images/blog.jpeg',
+        alt: 'project1',
+        heading: 'Blog Application',
+        description: 'Used CRUD'
+    },
+
+    {
+        key: 2,
+        src: process.env.PUBLIC_URL + '/images/notes.png',
+        alt: 'project2',
+        heading: 'Keeper App',
+        description: 'React based app'
+    },
+
+    {
+        key: 1,
+        src: process.env.PUBLIC_URL + '/images/list.jpeg',
+        alt: 'project3',
+        heading: 'To Do List',
+        description: 'React based App'
+    }
+]
+
+export default projectdetails
